2de8fb5cfef7ab1ba5e38df06d02456fa02c7b5e
Rafael Borges <rs.borges@novaims.unl.pt>
2025-12-18 19:29:13 +0000

    feat: Introduce LinkML schema, generated model, and documentation, replacing application profile configuration.

---
+++ sources/lookups/procurement_type_method_map.py
@@ -0,0 +1,18 @@
+PROCUREMENT_TYPE_METHOD_MAP = {
+  "Concurso público": "open",
+  "Concurso público simplificado": "open",
+  "Serviços sociais e outros serviços específicos": "open",
+  "Concurso de conceção simplificado": "open",
+  "Concurso limitado por prévia qualificação": "selective",
+  "Procedimento de negociação": "selective",
+  "Parceria para a inovação": "selective",
+  "Consulta Prévia": "limited",
+  "Consulta Prévia Simplificada": "limited",
+  "Consulta prévia ao abrigo do artigo 7º da Lei n.º 30/2021, de 21.05": "limited",
+  "Ao abrigo de acordo-quadro (art.º 259.º)": "limited",
+  "Ajuste Direto Regime Geral": "direct",
+  "Ajuste Direto Regime Geral ao abrigo do artigo 7º da Lei n.º 30/2021, de 21.05": "direct",
+  "Ao abrigo de acordo-quadro (art.º 258.º)": "direct",
+  "Contratação excluída II": "direct",
+  "Setores especiais – isenção parte II": "direct"
+}
